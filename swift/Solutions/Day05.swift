ff288adc77e54b2669b8eddfefbadf674fdcf6c5
Kushantha Attanayake <kushantha@utexas.edu>
2023-12-03 21:56:22 -0800

    Day 5 prep

---
+++ swift/Solutions/Day05.swift
@@ -0,0 +1,22 @@
+//
+//  Day05.swift
+//  advent_of_code_2023
+//
+//  Created by Kushantha Attanayake on 12/3/23.
+//
+
+import Foundation
+
+struct Day05: Day {
+    func part1Solution(input: [String]) -> String {
+        var answer = 1
+        return "\(answer)"
+    }
+    
+    func part2Solution(input: [String]) -> String {
+        var answer = 2
+        return "\(answer)"
+    }
+    
+    
+}
--- swift/main.swift
+++ swift/main.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-let day: Day = Day04()
+let day: Day = Day05()
 
-//day.validatePart1()
+day.validatePart1()
 //day.part1()
-day.validatePart2()
-day.part2()
+//day.validatePart2()
+//day.part2()
 
